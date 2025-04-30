73b33b2a938d207daa3e43ff7591079f81ff151c
Oskar Stark <oskarstark@googlemail.com>
2025-04-30 08:00:06 +0200

    Fix file header

--- src/Resolver/ResolverInterface.php
+++ src/Resolver/ResolverInterface.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 /**
- * This file is part of Storyblok-Api.
+ * This file is part of storyblok/php-content-api-client.
  *
- * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ * (c) Storyblok GmbH <info@storyblok.com>
+ * in cooperation with SensioLabs Deutschland <info@sensiolabs.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.

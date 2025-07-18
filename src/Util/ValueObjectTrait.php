9e3b2fb0530177cd7476b5b4e661863ca4faff3f
Kristian Hempel <kristian@staffbase.com>
2025-07-18 10:44:44 +0200

    fix

--- src/Util/ValueObjectTrait.php
+++ src/Util/ValueObjectTrait.php
@@ -378,7 +378,7 @@ trait ValueObjectTrait
         }
 
         if (null !== $regex) {
-            TrimmedNonEmptyString::fromString($regex)->toString();
+            TrimmedNonEmptyString::fromString($regex);
             Assert::regex($values[$key], $regex);
         }
 
@@ -405,7 +405,7 @@ trait ValueObjectTrait
         }
 
         if (null !== $regex) {
-            TrimmedNonEmptyString::fromString($regex)->toString();
+            TrimmedNonEmptyString::fromString($regex);
             Assert::regex($values[$key], $regex);
         }
 

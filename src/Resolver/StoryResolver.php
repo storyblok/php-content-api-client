4ea19c132b9bb1b189650d69b11250cd707afe7b
Silas Joisten <silasjoisten@proton.me>
2025-09-18 14:43:51 +0300

    fix: Resolve nested relations (#66)

--- src/Resolver/StoryResolver.php
+++ src/Resolver/StoryResolver.php
@@ -31,24 +31,30 @@ final readonly class StoryResolver implements ResolverInterface
             $relationMap[$relation['uuid']] = $relation;
         }
 
-        foreach ($target as &$value) {
-            if (\is_string($value) && \array_key_exists($value, $relationMap)) {
-                $value = $relationMap[$value];
 
-                continue;
-            }
+        // Resolve relations within the relation map first
+        $this->doResolve($relationMap, $relationMap);
 
-            if (\is_array($value) && \array_key_exists('id', $value) && \array_key_exists($value['id'], $relationMap)) {
-                $value = $relationMap[$value['id']];
-
-                continue;
-            }
-
-            if (\is_array($value)) {
-                $value = $this->resolve($value, $relations);
-            }
-        }
+        // Then resolve relations in the main target
+        $this->doResolve($target, $relationMap);
 
         return $target;
+    }
+
+    private function doResolve(array &$target, array &$relationMap): void
+    {
+        foreach ($target as $key => &$value) {
+            if ('uuid' === $key) {
+                continue;
+            }
+
+            if (\is_string($value) && isset($relationMap[$value])) {
+                $value = $relationMap[$value];
+            } elseif (\is_array($value) && isset($value['id'], $relationMap[$value['id']])) {
+                $value = $relationMap[$value['id']];
+            } elseif (\is_array($value)) {
+                $this->doResolve($value, $relationMap);
+            }
+        }
     }
 }
